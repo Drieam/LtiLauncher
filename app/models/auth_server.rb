d941e55d6d1e61950a08bef1e3c7dab8ec7eb299
Stef Schenkelaars <stef.schenkelaars@gmail.com>
2019-12-27 16:25:45 +0100

    Set CI with GitHub Actions

--- app/models/auth_server.rb
+++ app/models/auth_server.rb
@@ -2,6 +2,6 @@
 
 class AuthServer < ApplicationRecord
   validates :name, presence: true, uniqueness: true
-  validates :service_url, presence: true, format: URI::regexp(%w[http https])
+  validates :service_url, presence: true, format: URI.regexp(%w[http https])
   validates :client_id, presence: true
 end
