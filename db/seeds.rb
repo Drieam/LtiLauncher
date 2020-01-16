7631ef54b4d4f789f4f8897013a5d4cd81d7d142
Stef Schenkelaars <stef.schenkelaars@gmail.com>
2020-01-16 11:41:58 +0100

    Add SURF open ID configuration to seeds (#7)

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,7 +3,7 @@
 ##
 # Auth server based on the Auth0 SaaS
 # https://manage.auth0.com/dashboard/us/dev-xlgx8cg4/applications/16WTuSHVNEukJ1udo5U7RGf2P8WWnxCp/settings
-auth_server = AuthServer.create!(
+auth0_auth_server = AuthServer.create!(
   id: '5a90aa6a-e152-40ef-a1c6-60db9ce88e86',
   name: 'Auth0 Dev',
   openid_configuration_url: 'https://dev-xlgx8cg4.auth0.com/.well-known/openid-configuration',
@@ -13,9 +13,24 @@ auth_server = AuthServer.create!(
 )
 
 # Tool for certification suite
-auth_server.tools.create!(
+auth0_auth_server.tools.create!(
   id: 'a0f3de21-8f3b-48ae-a7a6-30185a91a956',
   client_id: 'cert',
   open_id_connect_initiation_url: 'https://ltiadvantagevalidator.imsglobal.org/ltiplatform/oidcinitialize.html',
   target_link_uri: 'https://ltiadvantagevalidator.imsglobal.org/ltiplatform/oidcredirecturl.html'
 )
+
+##
+# Auth server based on SURF's OIDC service
+# The configuration endpoint is not publicly available so make sure your IP is whitelisted @ SURF
+# To create the auth server, set the environment variable
+if ENV.key?('SURF_OIDC_CLIENT_SECRET')
+  AuthServer.create!(
+    id: 'f6837576-9bb1-4a4d-bd77-85f2ee66bb87',
+    name: 'Surf OIDC Test',
+    openid_configuration_url: 'https://oidc.dev.dlo.surf.nl/auth/realms/dlo/.well-known/openid-configuration',
+    client_id: 'lti-launcher',
+    client_secret: ENV.fetch('SURF_OIDC_CLIENT_SECRET'),
+    context_jwks_url: Rails.application.routes.url_helpers.keypairs_url
+  )
+end
